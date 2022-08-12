9f67d3b749f9a21d05b2f0e661fb32927c38189e
ZHENG <2082153514@qq.com>
2022-08-12 20:14:15 +0800

    添加创建基础文件

---
+++ Source/GroupD/GroupDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GroupDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GROUPD_API AGroupDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
